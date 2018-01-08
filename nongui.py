f0661edea865722fe09383c1b581f8242e0952e5
Matthew Jurenka <mattjurenka@users.noreply.github.com>
2018-01-07 22:00:47 -0800

    Added Command Line Interface

---
+++ nongui.py
@@ -0,0 +1,28 @@
+import sys
+from postscraper import PostScraper
+
+database = "testtable"
+
+f = open("credentials.txt")
+clientData = [f.readline for i in range(5)]
+f.close()
+
+if len(sys.argv) < 5:
+    print("Incorrect syntax, correct usage:")
+    print("nongui.py subreddit (new|hot|top) limit period")
+    sys.exit()
+
+subr = sys.argv[1]
+orgby = sys.argv[2]
+limit = sys.argv[3]
+period = sys.argv[4]
+if period[-1:] == "m":
+    period = int(period[:-1]) * 60
+else:
+    period = int(period)
+
+desired = ["fullname", "title", "url", "permalink", "subreddit", "created", "over_18", "score"]
+tables = ["after" + str(x * 30)  for x in range(13)]
+
+pscraper = PostScraper(database, clientData, tables, desired)
+pscraper.startScraping(period, subr, orgby, limit)
